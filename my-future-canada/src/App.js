48e4ef371e3b54edcc4f4849ee25f8330ad8446d
Michael Essex <112738703+messex1@users.noreply.github.com>
2024-06-29 12:19:31 -0600

    f

--- my-future-canada/src/App.js
+++ my-future-canada/src/App.js
@@ -1,8 +1,8 @@
 // src/App.js
 import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Banner from './components/Banner';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Banner from './Components/Banner';
 import './App.css';
 
 const App = () => {
