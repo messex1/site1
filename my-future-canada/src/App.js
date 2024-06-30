307a769b32dc38733bbdeec6f1c9db0590e3bf8f
Michael Essex <112738703+messex1@users.noreply.github.com>
2024-06-29 20:49:21 -0600

    r

--- my-future-canada/src/App.js
+++ my-future-canada/src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Banner from './Components/Banner';
+import Appbar from './Components/Appbar';
 import './App.css';
 
 const App = () => {
   return (
     <div className="container">
+      <Appbar />
       <Header />
       <main className="main">
         <Banner />
