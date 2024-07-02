bc92f91e3c481ffdb74e2f6f4f6461f6f6996c62
Michael Essex <112738703+messex1@users.noreply.github.com>
2024-07-02 00:55:24 -0600

    Update page.js

--- tim/app/page.js
+++ tim/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-4 sm:p-8 md:p-16 lg:p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p>My Future Canada</p>
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
@@ -20,7 +20,6 @@ export default function Home() {
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
-              className="dark:invert"
               width={100}
               height={24}
               priority
@@ -31,11 +30,12 @@ export default function Home() {
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="../public/my_future_canada_logo.svg"
-          alt="my future canada logo"
-          width={180}
-          height={37}
+          className="relative w-48 h-16 sm:w-60 sm:h-20 md:w-72 md:h-24 lg:w-96 lg:h-32 xl:w-[500px] xl:h-[167px]"
+          src="/my_future_canada_logo.svg"
+          alt="My Future Canada Logo"
+          layout="intrinsic"
+          width={500}
+          height={167}
           priority
         />
       </div>
@@ -47,16 +47,13 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-
-        {/* RCIC Login */}
-
-          <h2 className={`mb-3 text-2xl font-semibold`}>
+          <h2 className="mb-3 text-2xl font-semibold">
             RCIC Login{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
             Login to your account details.
           </p>
         </a>
@@ -67,15 +64,15 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          {/* RCIC Sign Up */}
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-           RCIC Sign Up{" "}
+          <h2 className="mb-3 text-2xl font-semibold">
+            RCIC Sign Up{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn more about the sign up process and how to become a RCIC!</p>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Learn more about the sign up process and how to become a RCIC!
+          </p>
         </a>
 
         <a
@@ -84,13 +81,13 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
+          <h2 className="mb-3 text-2xl font-semibold">
             Client Login{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
             Login in to your client account to view your application status.
           </p>
         </a>
@@ -101,14 +98,15 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
+          <h2 className="mb-3 text-2xl font-semibold">
             Client Sign Up{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-Click here to begin the client sign up process!          </p>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Click here to begin the client sign up process!
+          </p>
         </a>
       </div>
     </main>
